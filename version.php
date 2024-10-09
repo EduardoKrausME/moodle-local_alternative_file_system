a0c96d01b294e8dbb8097275474d0a3a5381b458
Eduardo Kraus <kraus@eduardokraus.com>
2024-10-09 10:34:02 -0300

    V 1.2.4

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2024082301;
+$plugin->version  = 2024100900;
 $plugin->requires = 2017051500;
-$plugin->release = '1.2.3';
+$plugin->release = '1.2.4';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'local_alternative_file_system';
